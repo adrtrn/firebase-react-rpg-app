9e648c7faf40c7083e6ee61389bfe31e65787cdf
olivecoil <adrian.s.tran@gmail.com>
2017-03-16 14:29:05 -0700

    Added: user sign-in, user profile card

--- src/Application.js
+++ src/Application.js
@@ -16,6 +16,28 @@ class App extends Component {
       user: null,
       users: {}
     };
+
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user })
+
+        this.usersRef = database.ref('/users')
+        this.userRef = this.usersRef.child(user.uid)
+
+        this.userRef.once('value').then((snapshot) => {
+          if (snapshot.val()) return
+          const userData = pick(user, ['displayName', 'photoURL', 'email'])
+          this.userRef.set(userData)
+        })
+
+        this.usersRef.on('value', (snapshot) => {
+          this.setState({ users: snapshot.val() })
+        })
+      }
+    })
   }
 
   render() {
@@ -26,7 +48,20 @@ class App extends Component {
         <header className="App--header">
           <h1>Social Animals</h1>
         </header>
-        <SignIn />
+        {
+          user
+          ? <div>
+              <section className="ProfileCards">
+                {
+                  map(users, (user, uid) => {
+                    return <ProfileCard key={uid} user={user} uid={uid} />
+                  })
+                }
+              </section>
+              <CurrentUser user={user} />
+            </div>
+          : <SignIn />  
+        }
       </div>
     );
   }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,16 +1,34 @@
-import React, { Component, PropTypes } from 'react';
-import FileInput from 'react-file-input';
-import { storage, database } from './firebase';
-import './ProfileCard.css';
+import React, { Component, PropTypes } from 'react'
+import FileInput from 'react-file-input'
+import { storage, database } from './firebase'
+import './ProfileCard.css'
 
 class ProfileCard extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event) {
+    const file = event.target.files[0]
+    console.log(file)
   }
 
   render() {
+    const { displayName, photoURL } = this.props.user
     return (
       <article className="ProfileCard">
+        <img 
+          className="ProfileCard--photo"
+          src={ photoURL }
+        />
+        <h3> { displayName } </h3>
+        <FileInput 
+          accept=".png,.gif,.jpg"
+          placeholder="Select an image:"
+          onChange={this.handleSubmit}
+        />
       </article>
     );
   }
@@ -25,4 +43,4 @@ ProfileCard.propTypes = {
   uid: PropTypes.string
 };
 
-export default ProfileCard;
+export default ProfileCard
